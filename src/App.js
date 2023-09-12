0ff83af379fb95c5f9a7b68dd7925db884009387
unknown <RicardoYalico>
2023-09-12 14:26:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let items=['Item 1','Item 2','Item 3','Item 4','Item 5'];
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hello</p>
+        <ul>
+     {items.map((item,index)=>{
+         return <li key={index}>{item}</li>
+     })}
+ </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
